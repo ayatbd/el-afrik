1063ef7b629bc9aa94761f779d615308e8de7a5b
ayatbd <albehestione@gmail.com>
2025-12-29 09:47:18 +0600

    upadated the rider tracking and the view details modal

--- src/components/modules/rider-tracking/ViewDriverDetails.tsx
+++ src/components/modules/rider-tracking/ViewDriverDetails.tsx
@@ -7,24 +7,10 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-
-import Swal from "sweetalert2";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
-import { X } from "lucide-react";
 
 export default function ViewDriverDetails() {
-  // handling the edit action
-  const handleEdit = () => {
-    console.log("Edit product with ID:");
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: "Updated Successfully",
-      showConfirmButton: false,
-      timer: 1500,
-    });
-  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -34,17 +20,16 @@ export default function ViewDriverDetails() {
       </DialogTrigger>
 
       <DialogContent className="sm:max-w-125 bg-white p-6 md:p-8">
-        <DialogHeader className="mb-6">
-          <DialogTitle className="text-2xl text-center">
-            Rider Details
-          </DialogTitle>
-        </DialogHeader>
-
         <div>
           <Card>
+            <DialogHeader className="mb-6">
+              <DialogTitle className="text-2xl text-center">
+                Rider Details
+              </DialogTitle>
+            </DialogHeader>
             <CardContent className="flex flex-col items-center px-6 pb-8">
               {/* Profile Image */}
-              <div className="relative w-40 h-40 mb-4 rounded-3xl overflow-hidden bg-gray-100">
+              <div className="relative w-40 h-40 mb-4 rounded-3xl overflow-hidden">
                 {/* Replace this src with your actual profile image */}
                 <Image
                   src="https://images.unsplash.com/photo-1556157382-97eda2d62296?q=80&w=400&auto=format&fit=crop"
@@ -66,19 +51,17 @@ export default function ViewDriverDetails() {
                   Location : 09 Arnulfo Crossing, Botsfordborough
                 </p>
               </div>
-
-              {/* Driving License Section */}
               <div className="w-full space-y-3">
-                <h3 className="text-lg font-medium text-gray-800 text-left">
-                  Driving License :
-                </h3>
-
-                <div className="relative w-full h-48 rounded-xl overflow-hidden border border-gray-200 bg-gray-50 shadow-sm">
+                <div className="relative flex items-center justify-center flex-col rounded-xl overflow-hidden gap-3">
+                  <h3 className="text-lg font-medium text-gray-800 text-left">
+                    Driving License :
+                  </h3>
                   {/* Replace this src with your actual license image */}
                   <Image
-                    src="https://images.unsplash.com/photo-1563829215-6800d11d9d43?q=80&w=600&auto=format&fit=crop"
+                    width={340}
+                    height={210}
+                    src="/images/dashboard/image.png"
                     alt="Driving License"
-                    fill
                     className="object-cover opacity-90 hover:scale-105 transition-transform duration-300"
                   />
 
