c067ad90f0eea78ec4d171c6608834d9cff6cb8d
ayatbd <albehestione@gmail.com>
2026-02-01 12:45:53 +0600

    changed the server link

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -21,7 +21,7 @@ interface RootState {
 }
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://15.223.245.199:5000/api/v1',
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://15.223.245.199/api/v1',
     prepareHeaders: (headers, { getState }) => {
         const state = getState() as RootState;
         let token = state.auth.accessToken;
